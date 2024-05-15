1a0d42576d7598e1282ed107bb49a7036cd25dcd
Hikmat Guliyev <hikmatasifli@gmail.com>
2024-05-15 11:36:04 +0400

    products page

--- src/pages/admin/products/products.jsx
+++ src/pages/admin/products/products.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import MainContext from '../../../context/context'
+import Table from '../../../components/admin/table/Table'
 
 const Products = () => {
-  return (
-    <div>products</div>
-  )
+    const { data } = useContext(MainContext)
+    return (
+        <div>
+            <Table items={data} />
+        </div>
+
+    )
 }
 
 export default Products
